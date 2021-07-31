48b8a458bb8c296818de99f430a6b97120ab732e
rotemetreex <60839395+rotemetreex@users.noreply.github.com>
2021-07-31 18:09:49 +0300

    cleaned up and formatted files

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 let playerTurn = 'X';
 
 function clickGridBox(id) {
-    
-    document.getElementById(id).innerText=playerTurn;
-    document.getElementById(id).style.pointerEvents='none';
 
-    if((document.getElementById('grid-item-1').innerText == document.getElementById('grid-item-2').innerText && 
-    document.getElementById('grid-item-2').innerText == document.getElementById('grid-item-3').innerText) &&  document.getElementById('grid-item-3').innerText != ''
-    || (document.getElementById('grid-item-4').innerText == document.getElementById('grid-item-5').innerText && 
-    document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-6').innerText) &&  document.getElementById('grid-item-6').innerText != ''
-    || (document.getElementById('grid-item-7').innerText == document.getElementById('grid-item-8').innerText && 
-    document.getElementById('grid-item-8').innerText == document.getElementById('grid-item-9').innerText) &&  document.getElementById('grid-item-9').innerText != ''
-    || (document.getElementById('grid-item-1').innerText == document.getElementById('grid-item-4').innerText && 
-    document.getElementById('grid-item-4').innerText == document.getElementById('grid-item-7').innerText) &&  document.getElementById('grid-item-7').innerText != ''
-    || (document.getElementById('grid-item-2').innerText == document.getElementById('grid-item-5').innerText && 
-    document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-8').innerText) &&  document.getElementById('grid-item-8').innerText != ''
-    || (document.getElementById('grid-item-3').innerText == document.getElementById('grid-item-6').innerText && 
-    document.getElementById('grid-item-6').innerText == document.getElementById('grid-item-9').innerText) &&  document.getElementById('grid-item-9').innerText != ''
-    || (document.getElementById('grid-item-1').innerText == document.getElementById('grid-item-5').innerText && 
-    document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-9').innerText) &&  document.getElementById('grid-item-9').innerText != ''
-    || (document.getElementById('grid-item-3').innerText == document.getElementById('grid-item-5').innerText && 
-    document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-7').innerText) &&  document.getElementById('grid-item-7').innerText != '') {
-        // console.log("Player " + playerTurn + " Wins!!")
+    document.getElementById(id).innerText = playerTurn;
+    document.getElementById(id).style.pointerEvents = 'none';
 
-        for(i = 0; i < document.getElementsByClassName('grid-item').length; i++) {
-            document.getElementsByClassName('grid-item')[i].style.pointerEvents='none';
+    if ((document.getElementById('grid-item-1').innerText == document.getElementById('grid-item-2').innerText &&
+        document.getElementById('grid-item-2').innerText == document.getElementById('grid-item-3').innerText) &&
+        document.getElementById('grid-item-3').innerText != ''
+        || (document.getElementById('grid-item-4').innerText == document.getElementById('grid-item-5').innerText &&
+            document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-6').innerText) &&
+        document.getElementById('grid-item-6').innerText != ''
+        || (document.getElementById('grid-item-7').innerText == document.getElementById('grid-item-8').innerText &&
+            document.getElementById('grid-item-8').innerText == document.getElementById('grid-item-9').innerText) &&
+        document.getElementById('grid-item-9').innerText != ''
+        || (document.getElementById('grid-item-1').innerText == document.getElementById('grid-item-4').innerText &&
+            document.getElementById('grid-item-4').innerText == document.getElementById('grid-item-7').innerText) &&
+        document.getElementById('grid-item-7').innerText != ''
+        || (document.getElementById('grid-item-2').innerText == document.getElementById('grid-item-5').innerText &&
+            document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-8').innerText) &&
+        document.getElementById('grid-item-8').innerText != ''
+        || (document.getElementById('grid-item-3').innerText == document.getElementById('grid-item-6').innerText &&
+            document.getElementById('grid-item-6').innerText == document.getElementById('grid-item-9').innerText) &&
+        document.getElementById('grid-item-9').innerText != ''
+        || (document.getElementById('grid-item-1').innerText == document.getElementById('grid-item-5').innerText &&
+            document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-9').innerText) &&
+        document.getElementById('grid-item-9').innerText != ''
+        || (document.getElementById('grid-item-3').innerText == document.getElementById('grid-item-5').innerText &&
+            document.getElementById('grid-item-5').innerText == document.getElementById('grid-item-7').innerText) &&
+        document.getElementById('grid-item-7').innerText != '') {
+
+        for (i = 0; i < document.getElementsByClassName('grid-item').length; i++) {
+            document.getElementsByClassName('grid-item')[i].style.pointerEvents = 'none';
         }
-        document.getElementById('winText').style.display='block';
-        document.getElementById('winText').innerText='Player ' + playerTurn + ' Wins!!'
-    } 
+        document.getElementById('winText').style.display = 'block';
+        document.getElementById('winText').innerText = 'Player ' + playerTurn + ' Wins!!'
+    }
 
-    if(playerTurn == 'X') {
+    if (playerTurn == 'X') {
         playerTurn = 'O';
     } else {
         playerTurn = 'X';
@@ -40,11 +47,11 @@ function clickGridBox(id) {
 
 function restartGame() {
     playerTurn = 'X';
-    for(i = 0; i < document.getElementsByClassName('grid-item').length; i++) {
-        document.getElementsByClassName('grid-item')[i].innerText='';
-        document.getElementsByClassName('grid-item')[i].style.pointerEvents='all';
+    for (i = 0; i < document.getElementsByClassName('grid-item').length; i++) {
+        document.getElementsByClassName('grid-item')[i].innerText = '';
+        document.getElementsByClassName('grid-item')[i].style.pointerEvents = 'all';
     }
-    document.getElementById('winText').style.display='none';
+    document.getElementById('winText').style.display = 'none';
 }
 
 
